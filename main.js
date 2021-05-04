554fa9769f21b240663a61c10e9a24d8ad4c86d1
Bruno Peselli <brunopeselli@gmail.com>
2021-05-04 19:45:16 -0300

    Refactoring

---
+++ main.js
@@ -0,0 +1,24 @@
+const downloadSheet = require('./lib/downloadSheet');
+const readExcel = require('./lib/readExcel');
+const newChrome = require('./lib/newChrome');
+const startChallenge = require('./lib/startChallenge');
+const executeChallenge = require('./lib/executeChallenge');
+
+(async function main() {
+    isDownloaded = await downloadSheet();
+    if(!isDownloaded) return console.log("Couldn't download sheet");
+
+    tasks = await readExcel();
+    if(!tasks) return console.log("Couldn't read Excel");
+
+    driver = await newChrome();
+    if(!driver) return console.log("Couldn't open Chrome");
+
+    challengeStarted = await startChallenge();
+    if(!challengeStarted) return console.log("Couldn't start challenge");
+    
+    result = await executeChallenge(tasks);
+    if(!result) return console.log("Couldn't finish challenge");
+
+    return console.log('Proccess finished succesfully!');
+})();
